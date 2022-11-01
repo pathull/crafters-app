7561185d9675d17ddbfa34a4d0cd96679b6a57b1
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-11-01 09:24:56 -0300

    feat(cors): cors was installed and frontend was connected to backend
    Get to test the connection among backend and frontend

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 
 import mainRoutes from '../routes/main-routes';
@@ -10,6 +11,11 @@ const app: Application = express();
 app.set('port', process.env.PORT || 8080);
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 app.use(morgan('dev'));
 
 app.use('/', mainRoutes);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  const goToLogin = async () => {
+    console.log('Dude you click on it');
+    // await fetch('http://localhost:8080/login');
+    const data = await fetch('http://localhost:8080/');
+    const hello = await data.json();
+    console.log(hello);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Welcome to Knitting Social Media App</h1>
+      <button onClick={goToLogin} type="button">
+        Login
+      </button>
+      <button type="button">Logout</button>
     </div>
   );
 }
