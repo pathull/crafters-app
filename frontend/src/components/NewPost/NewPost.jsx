1d7e259a17c610487c0d07dfb2879818676cb197
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-11-03 22:41:45 -0300

    feat(new-post): frontend new post component is being styled

--- frontend/src/components/NewPost/NewPost.jsx
+++ frontend/src/components/NewPost/NewPost.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
+import './NewPost.css';
+
 import { createNewPost } from '../../services/fetchData';
 
 export const NewPost = () => {
@@ -28,36 +30,56 @@ export const NewPost = () => {
   };
 
   return (
-    <div className="p-7">
-      <form onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="titlePost">Title</label>
-          <input type="text" name="title" id="titlePost" value={title} onChange={e => setTitle(e.target.value)} />
+    <section className="createNewPost__container">
+      <div className="formContainer">
+        <div className="p-7 formContainer__div">
+          <form className="formContainer__details" onSubmit={submitHandler}>
+            <div className="formContainer__title">
+              <input
+                className="formTitle__input focus:ring-0"
+                type="text"
+                name="title"
+                id="titlePost"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                placeholder=" "
+              />
+              <label className="formTitle__label" htmlFor="titlePost">
+                Title
+              </label>
+            </div>
+            <div className="formContainer__title">
+              <input
+                className="formTitle__input focus:ring-0"
+                type="text"
+                name="description"
+                id="descriptionPost"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+                placeholder=" "
+              />
+              <label className="formTitle__label" htmlFor="descriptionPost">
+                Description
+              </label>
+            </div>
+            <div className="formContainer__image">
+              <label htmlFor="postPicture">Image</label>
+              <input
+                type="file"
+                name="postPicture"
+                id="postPicture"
+                accept="image/*"
+                onChange={e => setImage(e.target.files[0])}
+              />
+            </div>
+            <div className="formContainer__btn">
+              <button className="submitButton__newPost" type="submit">
+                Create New Post
+              </button>
+            </div>
+          </form>
         </div>
-        <div>
-          <label htmlFor="descriptionPost">Description</label>
-          <input
-            type="text"
-            name="description"
-            id="descriptionPost"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="postPicture">Image</label>
-          <input
-            type="file"
-            name="postPicture"
-            id="postPicture"
-            accept="image/*"
-            onChange={e => setImage(e.target.files[0])}
-          />
-        </div>
-        <div>
-          <button type="submit">Create New Post</button>
-        </div>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 };
