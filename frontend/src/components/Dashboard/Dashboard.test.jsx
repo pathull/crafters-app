b75ef6fa0973b03e91c3aed00484586da4139109
Patrick Hull <patrick9hull4@gmail.com>
2022-11-18 17:19:38 -0800

    more testing

--- frontend/src/components/Dashboard/Dashboard.test.jsx
+++ frontend/src/components/Dashboard/Dashboard.test.jsx
@@ -1,9 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { expect, test } from '@jest/globals';
 // import userEvent from '@testing-library/user-event';
+import nock from 'nock';
+import '@testing-library/jest-dom';
+import { mockPostList } from '../../mocks/PostListMocks';
 import { Dashboard } from './Dashboard';
+import { BrowserRouter } from 'react-router-dom';
+import { env } from '../../helpers/env';
 
-describe('Test the Dashboard Component', () => {
-  test('Renders without crashing', () => {
-    render(<Dashboard />);
+describe('Dashboard fetching', () => {
+  nock(env.urlBase)
+    .persist()
+    .defaultReplyHeaders({
+      'access-control-allow-origin': '*',
+    })
+    .get('/listPosts')
+    .reply(200, mockPostList);
+
+  test('Should render correct data', async () => {
+    render(<Dashboard />, { wrapper: BrowserRouter });
+
+    await waitFor(() => {
+      expect(screen.findByAltText('a palm tree')).toBeDefined();
+    });
   });
 });
